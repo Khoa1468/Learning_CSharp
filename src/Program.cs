de2f22c94f652559f0d9b830e98251414a3b380f
Khoa1468 <69295064+Khoa1468@users.noreply.github.com>
2021-02-17 12:08:25 +0700

    Program CSharp

---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp
+{
+	class Program
+	{
+		/// <summary>
+		/// Hàm chạy đầu tiện khi chương trình đc biên dịch
+		/// </summary>
+		/// <param name="args"></param>
+		static void Khoa(string[] args)
+		{
+            #region Console.Write()
+
+            //Console.Write("Hello");
+            //Console.Write(10);
+            //Console.Write(10.9f);
+            //Console.Write(false);
+
+            #endregion
+
+            #region Console.WriteLine()
+
+            Console.WriteLine("Hello");
+            Console.WriteLine(10);
+            Console.WriteLine(10.9f);
+            Console.WriteLine(true);
+
+            #endregion
+
+            Console.ReadKey();
+		}
+    }
+}
---
+++ src/Sub_Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Learning_CSharp
+{
+    class Sub_Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.Write("a = {0}", a);
+            Console.ReadKey(true);
+        }
+    }
+}
