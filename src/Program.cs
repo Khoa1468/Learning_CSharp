cd3456cbc477fe7ffac253f1d8e15dd6cb943b33
Khoa1468 <69295064+Khoa1468@users.noreply.github.com>
2021-02-17 14:20:26 +0700

    New Update Program.cs File

--- src/Program.cs
+++ src/Program.cs
@@ -14,23 +14,11 @@ namespace Learning_CSharp
 		/// <param name="args"></param>
 		static void Main(string[] args)
 		{
-			#region Bien
-			int kieuSoNguyen = 10;
-			float kieuSoThuc = 10.9f;
-			string kieuChuoi = "Hello World!";
-			bool kieuLuanLy = true;
-			char kieuKyTu = 'K';
+			int num1 = 5;
+			int num2 = int.Parse(Console.ReadLine());
+			int sum = num1 + num2;
 
-			#endregion
-
-			int num1 = 10;
-			int num2 = 5;
-			int sum = num1 + num1;
-			
-
-			Console.WriteLine(kieuSoNguyen);
-			Console.WriteLine(kieuChuoi);
-			Console.WriteLine("a + b = c: {0} + {1} = {2}", num1, num2, sum);
+			Console.WriteLine(sum);
 
 			Console.ReadKey(true);
 		}
